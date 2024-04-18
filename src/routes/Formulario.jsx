64a71db84ec09ad8a24fb7885f3ed6bd916a36fc
jeffbaut13 <jeferssonabv@gmail.com>
2024-04-18 12:14:49 -0500

    ajustes form

--- src/routes/Formulario.jsx
+++ src/routes/Formulario.jsx
@@ -33,14 +33,37 @@ function Formulario() {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    // Aquí puedes enviar los datos a tu servidor utilizando fetch, axios u otra librería
+    // Por ejemplo:
+    fetch("http://newintegration.local/operacionmayo/comprar", {
+      method: "POST",
 
-    // Redirigir al usuario a la URL con los parámetros en la cadena de consulta
-    const queryString = Object.keys(formData)
-      .map((key) => key + "=" + encodeURIComponent(formData[key]))
-      .join("&");
-    window.location.href = `http://newintegration.local/operacionmayo/comprar?${queryString}`;
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Respuesta del servidor:", data);
+        // Aquí puedes hacer algo con la respuesta del servidor si es necesario
+
+        // Redirigir al usuario a la URL con los parámetros en la cadena de consulta
+        const queryString = Object.keys(formData)
+          .map((key) => key + "=" + formData[key])
+          .join("&");
+        window.location.href = `http://newintegration.local/operacionmayo/comprar?${queryString}`;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
